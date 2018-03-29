a5a8312d43a17e15994124779df16691fb0c42dc
Riley\RileyJayne <randers6@mail.umw.edu>
2018-03-29 10:18:38 -0400

    made addnode and nextword functions

--- word class.py
+++ word class.py
@@ -4,20 +4,32 @@
 class WordNode:
     def __init__(self, word):
         self.word=word
-        
+        self.nodes=[]      #this makes an instance variable: different list for each word? 
         
           
-    def addNode(self, word): #this is where I am stuck. I know I need to use word but I dont know how 
-        word_2=WordNode("I")
+    def addNode(self, nodes): 
+        self.nodes.append(nodes)
         
     def nextWord(self):
-        return('{}{}'.format(self.word_1,self.word_2))
+        return('{}{}'.format(self.word,self.nodes))  #this gives back all the words that can possibly come after
 
 
-    #def printWord(self):
+    def printWord(self):
+        print()
         
         
 word_1=WordNode("I")
-word_2=WordNode("am")
+word_1.addNode("am")
+word_1.addNode("want")
 
-print(word_1.addNode())
+word_2=WordNode("am")
+word_2.addNode("hungry")
+
+word_3=WordNode("hungry")
+
+word_4=WordNode("want")
+word_4.addNode("food")
+
+word_5=WordNode("food")
+
+print(word_1.nextWord())
