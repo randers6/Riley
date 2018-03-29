0dc70304882cd4cf37e574f4a0d136897fe7da0b
Riley\RileyJayne <randers6@mail.umw.edu>
2018-03-29 12:08:02 -0400

    made words in a list and added for loop

--- word class.py
+++ word class.py
@@ -1,35 +1,53 @@
-
+import random
 
 
 class WordNode:
     def __init__(self, word):
         self.word=word
-        self.nodes=[]      #this makes an instance variable: different list for each word? 
+        self.nodes=[]      #this makes an instance variable
         
           
     def addNode(self, nodes): 
         self.nodes.append(nodes)
         
-    def nextWord(self):
-        return('{}{}'.format(self.word,self.nodes))  #this gives back all the words that can possibly come after
-
+    def nextWord(self):#check if empty 
+        if self.nodes==[]:
+            return(None)
+        else:
+            
+            r=random.choice(self.nodes)
+            return(r) #this gives back all the words that can possibly come after I wat just one word 
+#am going to use a random number generator to pick a random slot in self.nodes
 
     def printWord(self):
-        print()
+        print(self.word)
+
+     
+word=[]      
+word.append(WordNode("I")) # this worked before random pick why???something to do with importing random? 
+word.append(WordNode("am"))
+word.append(WordNode("hungry"))
+word.append(WordNode("want"))
+word.append(WordNode("food"))
+
+word[0].addNode(word[1])
+word[0].addNode(word[3])
+word[1].addNode(word[2])
+word[3].addNode(word[4])
+
+print(word[1].nextWord())
+
+
+Current=word[1]
+for ii in range(0,3):
+    Current.printWord()
+    Current = Current.nextWord()
+
+    if Current==None:
+        break
+    
         
-        
-word_1=WordNode("I")
-word_1.addNode("am")
-word_1.addNode("want")
 
-word_2=WordNode("am")
-word_2.addNode("hungry")
 
-word_3=WordNode("hungry")
 
-word_4=WordNode("want")
-word_4.addNode("food")
-
-word_5=WordNode("food")
-
-print(word_1.nextWord())
+#I need this pick one word from self.nodes then loop back and have that ode be the word. Then continue and print that at the end. 
